17985a475549a342a809f1ffb670a06ffaa74a64
Arjun <arjun2594@gmail.com>
2014-11-16 11:22:31 +0530

    Minor Changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,40 +6,40 @@ import XMonad.Util.EZConfig(additionalKeys)
 import qualified XMonad.StackSet as W
 import System.IO
 import Control.Monad
+import XMonad.Hooks.ManageHelpers
 
-myWorkspaces = ["main", "web", "dev","misc", "media", "float", "chat"]
+myWorkspaces = ["main", "web", "dev","media", "misc", "float", "vbox"]
 
 -- Define the workspace an application has to go to
 myManageHook = composeAll . concat $
     [
-          -- Applications that go to web
+    
       [ className =? a --> doF (W.shift "web") | a <- myClassWebShifts  ]
-         -- Applications that go to chat
     , [ resource  =? b --> doF (W.shift "main") | b <- myClassMainShifts ]   
-    , [ resource  =? c --> doF (W.shift "chat") | c <- myClassChatShifts ]
+    , [ className =? c --> doF (W.shift "vbox") | c <- myClassVBoxShifts ]
     , [ resource  =? d --> doF (W.shift "dev") | d <- myClassDevShifts ]
     , [ resource  =? e --> doF (W.shift "float") | e <- myClassFloatShifts ]
     , [ resource  =? f --> doF (W.shift "misc") | f <- myClassMiscShifts ] 
     , [ resource  =? g --> doF (W.shift "media") | g <- myClassMediaShifts ]  
-   
     ]
     where
        --  viewShift = doF . liftM2 (.) W.greedyView W.shift
-        myClassMainShifts = ["gnome-terminal-server"]
+        myClassMainShifts = ["gnome-terminal"]
         myClassWebShifts  = ["Firefox"]
-        myClassChatShifts = ["Irc"]
+        myClassVBoxShifts = ["VirtualBox"]
         myClassDevShifts = ["emacs"]
         myClassFloatShifts = ["gimp"]
         myClassMiscShifts = ["nautilus"]
 	myClassMediaShifts = ["vlc"]
 
+
 main = do
   
-  xmproc <- spawnPipe "/bin/xmobar /home/arjun/.xmobarrc"
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/arjun/.xmobarrc"
   xmonad $ defaultConfig
     {
       borderWidth = 0
-    , manageHook = manageDocks <+> myManageHook
+    , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> myManageHook
                    <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
@@ -50,7 +50,6 @@ main = do
     , startupHook = do
         spawn "firefox"
         spawn "emacs"
-        spawn "nautilus"
         spawn "gnome-terminal"
     , workspaces = myWorkspaces
     , terminal = "gnome-terminal"
